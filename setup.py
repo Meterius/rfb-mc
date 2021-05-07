42aeecaf9dae41b675f302a76cea276428e62cad
Meterius <Meterius@users.noreply.github.com>
2021-05-07 16:13:24 +0200

    0.0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rfb_mc",
-    version="0.0.18",
+    version="0.0.19",
     author="Jonah Leander Hoff",
     author_email="jonah-hoff@outlook.com",
     description="Performs model counting using restrictive formulas",
